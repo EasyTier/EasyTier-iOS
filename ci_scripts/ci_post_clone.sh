1485a6c20f64603566eaed3910350b03daa3c678
Chenx Dust <chenx_dust@outlook.com>
2026-01-11 14:43:42 +0800

    ci: fix build

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+set -e
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -t aarch64-apple-ios
+
+export NONINTERACTIVE=1
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install protobuf
